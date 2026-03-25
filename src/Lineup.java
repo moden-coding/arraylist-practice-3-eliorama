52b9cfe7634f15001a6c449d587d554008b9f819
eliorama <28eancowitz@gannacademy.org>
2026-03-25 14:04:20 -0400

    done!

--- src/Lineup.java
+++ src/Lineup.java
@@ -7,17 +7,36 @@ public class Lineup {
 
         // Step 2: Add exactly 10 player names using .add()
         // The first 5 you add will be the starters; the last 5 will be the bench
+        roster.add("Ben");
+        roster.add("Shalev");
+        roster.add("Shaya");
+        roster.add("Henry");
+        roster.add("Brandon");
+        roster.add("Ori");
+        roster.add("David");
+        roster.add("Eli");
+        roster.add("Tom");
+        roster.add("Guy");
 
 
         // Step 3: Print a "STARTERS" label, then loop through and print the first 5 players
         // Each player should be numbered (Player 1, Player 2, etc.)
-
+        System.out.println("--- STARTERS ---");
+        for(int i=0; i <= roster.size()-6; i++){
+            System.out.println("Player "+ (i+1) + ":" + roster.get(i));
+        }
+    
 
         // Step 4: Print a "BENCH" label, then loop through and print the last 5 players
         // Continue your numbering (Player 6, Player 7, etc.) OR restart at 1 — your choice
+        System.out.println("--- BENCH ---");
+        for(int i = 5; i<roster.size(); i++ ){
+            System.out.println("Player "+ (1+i) + ":" + roster.get(i));
 
+        }
 
         // Step 5: Print the total roster size using .size()
+        System.out.println("Total Roster: " + roster.size() + " Players");
 
     }
 }
